8f4dbb2ab24a9e00293c4a608e715cbc5d1cffb4
lim hyo yeol <why4517@naver.com>
2023-06-09 18:22:12 +0900

    Step05 에 자료실 페이징 처리 구현함

--- Step05Final/src/main/java/test/file/dto/FileDto.java
+++ Step05Final/src/main/java/test/file/dto/FileDto.java
@@ -8,12 +8,15 @@ public class FileDto {
 	private String saveFileName;
 	private long fileSize; //파일의 크기는 byte 단위로 큰 정수를 쓰기 때문에 long int 를 사용한다.
 	private String regdate;
-
+	//페이징 처리에 관련된 필드
+	private int startRowNum;
+	private int endRowNum;
+	
 	//생성자 
 	public FileDto() {}
 
 	public FileDto(int num, String writer, String title, String orgFileName, String saveFileName, long fileSize,
-			String regdate) {
+			String regdate, int startRowNum, int endRowNum) {
 		super();
 		this.num = num;
 		this.writer = writer;
@@ -22,9 +25,9 @@ public class FileDto {
 		this.saveFileName = saveFileName;
 		this.fileSize = fileSize;
 		this.regdate = regdate;
+		this.startRowNum = startRowNum;
+		this.endRowNum = endRowNum;
 	}
-
-
 
 	public int getNum() {
 		return num;
@@ -82,4 +85,19 @@ public class FileDto {
 		this.regdate = regdate;
 	}
 
+	public int getStartRowNum() {
+		return startRowNum;
+	}
+
+	public void setStartRowNum(int startRowNum) {
+		this.startRowNum = startRowNum;
+	}
+
+	public int getEndRowNum() {
+		return endRowNum;
+	}
+
+	public void setEndRowNum(int endRowNum) {
+		this.endRowNum = endRowNum;
+	}
 }
